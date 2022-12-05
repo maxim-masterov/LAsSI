fce4e301abf01c49a44c30f792702778515ca718
maxim-masterov <maxim.vadimovich@gmail.com>
2022-12-05 19:13:23 +0100

    Cleaning

--- analysis/omp_analysis.py
+++ analysis/omp_analysis.py
@@ -44,23 +44,6 @@ class OMPAnalysis(Executor):
             self.run_repetitive_tests(batch_file_name, full_tmp_path, num_threads,
                                       successful_jobs['id'], successful_jobs['dir'], successful_jobs['threads'])
 
-            # max_rep = self.get_src_data().get_num_repetitions()
-            # for rep in range(0, max_rep):
-            #     io_manager.print_info('Iteration: ' + str(rep + 1) + '/' + str(max_rep))
-            #
-            #     # Submit job script
-            #     # Change to test directory
-            #     os.chdir(full_tmp_path)
-            #     job_id, state = self.get_batch_data().submit_job_script(batch_file_name)
-            #     # Change back to working directory
-            #     os.chdir(self.get_root_dir_name())
-            #
-            #     # Skip failing jobs (won't be used in the report)
-            #     if (job_id is not None) and (state != 'CANCELLED'):
-            #         successful_jobs['id'].append(job_id)
-            #         successful_jobs['dir'].append(full_tmp_path)
-            #         successful_jobs['threads'].append(num_threads)
-
             self.report_end_of_test(counter, num_tests)
 
         report = GenericReport()
